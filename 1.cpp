e4c9c33e9f9510767a4d358d1881472507646f97
Hemantha Kumara E K <125449369+hemanthek73@users.noreply.github.com>
2024-07-01 07:17:37 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,67 +1,113 @@
-#include <GL/glut.h>
-#include <stdio.h>   
-#include <stdlib.h> 
 
-
-void drawLine(int x0, int y0, int x1, int y1) {
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-    int sx = (x0 < x1) ? 1 : -1;
-    int sy = (y0 < y1) ? 1 : -1;
-    int err = dx - dy;
-
-    while (1) {
-        glBegin(GL_POINTS);
-        glVertex2i(x0, y0);
-        glEnd();
-
-        if (x0 == x1 && y0 == y1) break;
-
-        int e2 = 2 * err;
-        if (e2 > -dy) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y0 += sy;
-        }
-    }
+#include<GL/glut.h>
+#include<stdio.h>
+int x1, y1, x2, y2;
+void draw_pixel(int x, int y)
+{
+ glColor3f(1.0,0.0,0.0);
+ glBegin(GL_POINTS);
+ glVertex2i(x, y);
+ glEnd();
 }
-
-void display() {
-    int x1, x2, y1, y2;
-
-    printf("Enter coordinates for x1 and y1: ");
-    scanf("%d %d", &x1, &y1);
-    printf("Enter coordinates for x2 and y2: ");
-    scanf("%d %d", &x2, &y2);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glColor3f(1.0f, 1.0f, 1.0f);
-    drawLine(x1, y1, x2, y2);
-
-    glFlush();
+void bresenhams_line_draw(int x1, int y1, int x2, int y2)
+{
+ int dx = x2 - x1; 
+ int dy = y2 - y1; 
+ int m = dy/dx; 
+ if (m < 1)
+ {
+ int decision_parameter = 2*dy - dx;
+ int x = x1; 
+ int y = y1; 
+ if (dx < 0) 
+ {
+ x = x2; 
+ y = y2;
+ x2 = x1;
+ }
+ draw_pixel (x, y); 
+ while (x < x2) 
+ {
+ if (decision_parameter >= 0)
+ {
+ x = x+1;
+ y = y+1;
+ decision_parameter = decision_parameter + 2*dy - 2*dx * (y+1 - y);
+ }
+ else
+ {
+ x = x+1;
+ y = y;
+ decision_parameter = decision_parameter + 2*dy - 2*dx * (y - y);
+ }
+ draw_pixel (x, y);
+ }
+ }
+else if (m > 1)
+ {
+ int decision_parameter = 2*dx - dy;
+ int x = x1;
+ int y = y1; 
+ if (dy < 0)
+ {
+ x = x2;
+ y = y2;
+ y2 = y1;
+ }
+ draw_pixel (x, y);
+ while (y < y2)
+ {
+ if (decision_parameter >= 0)
+ {
+ x = x+1;
+ y = y+1;
+ decision_parameter = decision_parameter + 2*dx - 2*dy * (x+1 - x);
+ }
+ else
+ {
+ y = y+1;
+ x = x;
+ decision_parameter = decision_parameter + 2*dx - 2*dy * (x- x);
+ }
+ draw_pixel(x, y);
+ }
+ }
+ else if (m == 1)
+ {
+ int x = x1;
+ int y = y1;
+ draw_pixel (x, y);
+ while (x < x2)
+ {
+ x = x+1;
+y = y+1;
+ draw_pixel (x, y);
+ }
+ }
 }
-
-
-void initializeOpenGL(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Bresenham's Line Algorithm");
-
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 800, 0, 600);
-
-    glutDisplayFunc(display);
+void init()
+{
+ glClearColor(1,1,1,1);
+ gluOrtho2D(0.0, 500.0, 0.0, 500.0); 
 }
-
-int main(int argc, char** argv) {
-    initializeOpenGL(argc, argv);
-    glutMainLoop();
-    return 0;
+void display()
+{
+ glClear(GL_COLOR_BUFFER_BIT);
+ bresenhams_line_draw(x1, y1, x2, y2);
+ glFlush();
+}
+int main(int argc, char **argv)
+{
+ printf( "Enter Start Points (x1,y1)\n");
+ scanf("%d %d", &x1, &y1);                          
+ printf( "Enter End Points (x2,y2)\n");
+ scanf("%d %d", &x2, &y2);                     
+ glutInit(&argc, argv);                                
+ glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);   
+ glutInitWindowSize(500, 500); 
+ glutInitWindowPosition(220, 200);                                    
+ glutCreateWindow("Bresenham's Line Drawing");               
+ init();                                                                                                
+ glutDisplayFunc(display);                                                            
+ glutMainLoop();                                                                                
 }
